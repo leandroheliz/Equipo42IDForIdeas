afdd31f84045af34e38ead2252bbc38d4a236e0d
Leandro Heliz <leandroheliz@gmail.com>
2022-01-08 21:57:29 -0300

    rutas

--- src/Components/Pages/Homebanking.js
+++ src/Components/Pages/Homebanking.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Homebanking = () => {
   return (
-    <div>
-      homebanking 123
+    <div className="container">
+      <div>
+        menu contenido
+      </div>
+      <div className="row">
+        <div className="col">col sidebar</div>
+        <div className="col">col contenido</div>
+        </div>
     </div>
   )
 }
