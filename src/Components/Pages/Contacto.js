0185a1d151025559a455bd5a046bc5f07d7cf43e
Leandro Heliz <leandroheliz@gmail.com>
2022-01-09 18:10:12 -0300

    home

--- src/Components/Pages/Contacto.js
+++ src/Components/Pages/Contacto.js
@@ -1,9 +1,20 @@
-import React from 'react'
 
 const Contacto = () => {
   return (
-    <div>
-      contacto
+    <div className="container text-center">
+      <div className="row">
+        <div className="col colFooter">
+          <span><i className="fas fa-phone-volume mb-2"></i></span>
+          <span><i className="fas fa-map-marker-alt mb-2"></i></span>
+          <span><i className="fas fa-envelope mb-2"></i></span>
+        </div>
+        <div className="col colFooter">
+          <h3>Contactanos</h3>
+    <input type="text" class="inputContacto mb-3" placeholder="Nombre y Apellido"/>
+        <button type="submit">Enviar</button>
+        </div>
+      </div>
+      <br/>
     </div>
   )
 }
