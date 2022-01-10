dbddaa142bcd43224051062d43ac9aa464bd7127
FarBusC4 <marcosfer243@gmail.com>
2022-01-10 20:49:59 -0300

    homebanking

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,45 +1,53 @@
-
 const Footer = () => {
   return (
     <div className="footer text-center">
-        <br/>
+      <br />
       <div className="row">
         <div className="col colFooter">
-            <h4>Ayuda y guías</h4>
-            <span>App Plus Bank</span>
-            <span>Asesoramiento sobre deudas con atraso</span>
-            <span>ATM personas con discapacidad visual</span>
-            <span>Centro de seguridad</span>    
+          <h4>Ayuda y guías</h4>
+          <span>App Plus Bank</span>
+          <span>Asesoramiento sobre deudas con atraso</span>
+          <span>ATM personas con discapacidad visual</span>
+          <span>Centro de seguridad</span>
         </div>
         <div className="col colFooter">
-        <span><h4>Conocenos</h4></span>
-        <span>Autoridades</span>
-        <span>Carta Orgánica</span>
-        <span>Estados Financieros</span>
-        <span>Prensa y noticias</span>
-   
+          <span>
+            <h4>Conocenos</h4>
+          </span>
+          <span>Autoridades</span>
+          <span>Carta Orgánica</span>
+          <span>Estados Financieros</span>
+          <span>Prensa y noticias</span>
         </div>
         <div className="col colFooter">
-        <span><h4>Información legal</h4></span>
-        <span>Atención al usuario</span>
-        <span>Información al usuario</span>
-        <span>Régimen de transparencia del BCRA</span>
-        <span>Alyc</span>
+          <span>
+            <h4>Información legal</h4>
+          </span>
+          <span>Atención al usuario</span>
+          <span>Información al usuario</span>
+          <span>Régimen de transparencia del BCRA</span>
+          <span>Alyc</span>
         </div>
         <div className="col colFooter">
-          <span><h4>Otros sitios de Interés</h4></span>
+          <span>
+            <h4>Otros sitios de Interés</h4>
+          </span>
           <span>Conoce más sobre el Equipo 42</span>
           <span>Developers del proyecto</span>
           <span>Skills de Plus Bank</span>
           <span>Más sobre el proyecto</span>
         </div>
       </div>
-      <br/>
-      <span className="text-white">Términos y condiciones de navegabilidad de sitio web</span>
-      <hr/>
-           <span className="text-white">&copy; {new Date().getFullYear()}  Copyright | Plus Bank & Grupo 42 S.A</span>
+      <br />
+      <span className="text-white">
+        Términos y condiciones de navegabilidad de sitio web
+      </span>
+      <hr />
+      <span className="text-white mb-3 ">
+        &copy; {new Date().getFullYear()} Copyright | Plus Bank & Grupo 42 S.A
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/Components/SidebarHomebanking.js
+++ src/Components/SidebarHomebanking.js
@@ -3,40 +3,39 @@ import { Link } from "react-router-dom";
 
 const SidebarHomebanking = () => {
   return (
-      <div className="row">
-<div className="col-4 sidebar">
-  <div className="logo text-center">
-    <p>Bienvenido @usuario</p>
-  </div>
-  <div className="menu p-1">
-    <Link to="/perfilusuario" className="text-decoration-none text-white">
-      <i className="fas fa-user"></i> Mi Perfl
-    </Link>
-    <Link to="/" className="text-decoration-none text-white">
-      <i className="fas fa-wallet"></i> Saldo
-    </Link>
-    <Link to="/" className="text-decoration-none text-white">
-      <i className="fas fa-file-invoice-dollar"></i> Cuentas
-    </Link>
-    <Link to="/" className="text-decoration-none text-white">
-      <i className="far fa-credit-card"></i> Tarjetas
-    </Link>
-    <Link to="/" className="text-decoration-none text-white">
-      <i className="fas fa-tachometer-alt"></i> Movimientos
-    </Link>
-    <Link to="/" className="text-decoration-none text-white">
-      <i className="fas fa-cogs"></i> Configuración
-    </Link>
-    <div className="salir p-4">
-    <Link to="/" className="text-decoration-none text-white">
-      <i class="fas fa-reply-all"></i> Salir
-    </Link>
+    <div className="row">
+      <div className="col-4 sidebar bg-dark h-100">
+        <div className="logo text-center">
+          <p>Bienvenido @usuario</p>
+        </div>
+        <div className="menu p-1">
+          <Link to="/perfilusuario" className="text-decoration-none text-white">
+            <i className="fas fa-user"></i> Mi Perfl
+          </Link>
+          <Link to="/" className="text-decoration-none text-white">
+            <i className="fas fa-wallet"></i> Saldo
+          </Link>
+          <Link to="/" className="text-decoration-none text-white">
+            <i className="fas fa-file-invoice-dollar"></i> Cuentas
+          </Link>
+          <Link to="/" className="text-decoration-none text-white">
+            <i className="far fa-credit-card"></i> Tarjetas
+          </Link>
+          <Link to="/" className="text-decoration-none text-white">
+            <i className="fas fa-tachometer-alt"></i> Movimientos
+          </Link>
+          <Link to="/" className="text-decoration-none text-white">
+            <i className="fas fa-cogs"></i> Configuración
+          </Link>
+          <div className="salir p-4">
+            <Link to="/" className="text-decoration-none text-white">
+              <i class="fas fa-reply-all"></i> Salir
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
-  )
-}
+  );
+};
 
 export default SidebarHomebanking;
-
