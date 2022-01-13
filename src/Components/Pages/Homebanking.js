3638c2daf483babb07138b72643d9438a1ba6ac5
FarBusC4 <marcosfer243@gmail.com>
2022-01-13 12:31:03 -0300

    Homebanking

--- src/Components/Pages/Homebanking.js
+++ src/Components/Pages/Homebanking.js
@@ -4,28 +4,30 @@ import Logo from "../Logo/Logo";
 import Actividad from "../Actividad/Actividad";
 import Saldo from "../Saldo/Saldo";
 import SidebarHomebanking from "../SidebarHomebanking";
+import { Outlet } from "react-router-dom";
 
 const Homebanking = () => {
   return (
     <>
-    <div className="row">
-    <NavHomebanking />
-      <div className="col-4 col-sm-2">
-      <SidebarHomebanking/>
-      </div>
-      <div className="col container d-flex text-center">
-            <div className="col colHomebanking">
-               <Saldo />
-                <br/>
-                <Logo />
-            </div>
-            <div className="col">
-              <br/>
+      <div className="row">
+        <NavHomebanking />
+        <div className="col-4 col-sm-2">
+          <SidebarHomebanking />
+        </div>
+        <div className="col container d-flex text-center">
+          <div className="col colHomebanking">
+            <Saldo />
+            <br />
+            <Logo />
+          </div>
+          <div className="col">
+            <br />
             <Actividad />
-            </div>
+          </div>
+          <Outlet></Outlet>
+        </div>
       </div>
-    </div>
-      </>
+    </>
   );
 };
 
--- src/Routes/Rutas.js
+++ src/Routes/Rutas.js
@@ -1,18 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import DashboardRoutes from './DashboardRoutes';
-import Homebanking from '../Components/Pages/Homebanking';
-import PerfilUsuario from '../Components/PerfilUsuario/PerfilUsuario';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import DashboardRoutes from "./DashboardRoutes";
+import Homebanking from "../Components/Pages/Homebanking";
+import PerfilUsuario from "../Components/PerfilUsuario/PerfilUsuario";
+//import HomebankingInicio from "../Components/Pages/Homebanking_inicio";
 
 const Rutas = () => {
   return (
     <>
       <Routes>
-        <Route path="/homebanking" element={<Homebanking />} />
-        <Route path="/perfilusuario" element={<PerfilUsuario />} />
-        <Route path="/*" element={<DashboardRoutes/>} />
+        <Route path="/homebanking" element={<Homebanking />}>
+          <Route path=":perfilusuario" element={<PerfilUsuario />}></Route>
+          <Route path=":saldo" element={<PerfilUsuario />}></Route>
+          <Route path=":enviardinero" element={<PerfilUsuario />}></Route>
+          <Route path=":tarjetas" element={<PerfilUsuario />}></Route>
+          <Route path=":movimientos" element={<PerfilUsuario />}></Route>
+          <Route path=":configuracion" element={<PerfilUsuario />}></Route>
+        </Route>
+        <Route path="/*" element={<DashboardRoutes />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
 export default Rutas;
