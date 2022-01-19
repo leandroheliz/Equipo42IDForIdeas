bb7ea644689fa305abbf57485563d7d1593adb11
Leandro Heliz <leandroheliz@gmail.com>
2022-01-18 21:53:16 -0300

    404

--- src/Components/Pages/PagesBanco/Error404.js
+++ src/Components/Pages/PagesBanco/Error404.js
@@ -6,8 +6,6 @@ const Error404 = () => {
       <div className="row">
         <div className="col">
       <h1>Error 404</h1>
-      <br/>
-      <iframe src="https://giphy.com/embed/qHXSYtyW0kANmLLzcG" width="480" height="426" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
       <br/><br/>
       <Link to="/"><button className="btn"><h3>Volver al Home</h3></button></Link>
         </div>
