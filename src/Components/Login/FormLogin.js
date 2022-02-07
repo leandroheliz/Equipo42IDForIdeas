fb1e322ba38c3ab21c56672e4e96a57d41013c48
maurogoyeneche <goyenechemauro@gmail.com>
2022-02-07 00:16:42 -0300

    install axios &  hotfix FormLogin

--- src/Components/Login/FormLogin.js
+++ src/Components/Login/FormLogin.js
@@ -1,36 +1,28 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { React, useState } from "react";
-import { Link } from "react-router-dom";
-import cors from "cors";
+// import { React, useState } from "react";
+// import { Link } from "react-router-dom";
+// import cors from "cors";
+import axios from "axios";
 
 const FormLogin = () => {
+  const getToken = (user) => {
+    axios
+      .post("https://backend-equipo42-idf-or-ideas.vercel.app/user/token", {
+        email: user.email,
+        password: user.password,
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <Formik
         initialValues={{ formLoginEmail: "", formLoginPass: "" }}
-        validate={(values) => {
-          let errores = {};
-
-          //Validacion email
-          if (!values.formLoginEmail) {
-            errores.formLoginEmail = "Por favor ingresa un email";
-          } else if (
-            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
-              values.formLoginEmail
-            )
-          ) {
-            errores.formLoginEmail = "Ingrese un formato de email valido";
-          }
-
-          //Validacion password
-          if (!values.formLoginPass) {
-            errores.formLoginPass = "Por favor ingresa una password";
-          } else if (!/^[0-9]{3}$/.test(values.formLoginPass)) {
-            errores.formLoginPass = "Ingrese una contraseña valida";
-          }
-
-          return errores;
-        }}
         onSubmit={(values, { resetForm }) => {
           resetForm();
           console.log("Enviado");
@@ -38,27 +30,7 @@ const FormLogin = () => {
             email: values.formLoginEmail,
             password: values.formLoginPass,
           };
-
-          const getToken = () => {
-            fetch(
-              "https://backend-equipo42-idf-or-ideas.vercel.app/user/token",
-              {
-                method: "POST",
-                mode: "no-cors",
-                headers: {
-                  Accept: "application/json",
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  email: values.formLoginEmail,
-                  password: values.formLoginPass,
-                }),
-              }
-            ).then((res) => {
-              console.log(res);
-            });
-          };
-          getToken();
+          getToken(user);
         }}
       >
         {({ errors }) => (
